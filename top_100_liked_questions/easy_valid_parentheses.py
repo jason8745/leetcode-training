bc48d1d54ae0fa4fc07810f5a2e869713f4fe9be
jason8745 <41944427+jason8745@users.noreply.github.com>
2022-06-16 03:49:09 +0800

    6/16 update

--- top_100_liked_questions/easy_valid_parentheses.py
+++ top_100_liked_questions/easy_valid_parentheses.py
@@ -8,9 +8,8 @@
 - 檢查字串中,有對應關係的符號(ex: () {})是否左右數量正確,且符合語法順序
 解題思路
 1.先透過dict儲存符號的對應關係
-2.因為符號有先後關係,所以一定要從open_char開始
-3.因為符號的合法順序是LIFO,所以這裡可以使用stack
-4.stack and i == char_dict[stack[-1]] 是指要同時符合stack不為空,即比對stack最外層
+2.因為符號的合法順序是LIFO,所以這裡可以使用stack
+3.stack and i == char_dict[stack[-1]] 是指要同時符合stack不為空,即比對stack最外層
 TakeAway
 - 如果資料有pair關係,可以透過dict維護
 - 如果資料有LIFO特性,可以使用stack
@@ -18,12 +17,11 @@ TakeAway
 class Solution:
     def isValid(self, s: str) -> bool:
         char_dict={"(":")", "[": "]",  "{": "}"}
-        open_char = ["(","[","{"]
         stack=[]
         for i in s:
-            if i in open_char:
+            if i in char_dict.keys():
                 stack.append(i)
-            elif stack and i == char_dict[stack[-1]]:
+            elif stack and  i == char_dict[stack[-1]]:
                 stack.pop()
             else:
                 return False
